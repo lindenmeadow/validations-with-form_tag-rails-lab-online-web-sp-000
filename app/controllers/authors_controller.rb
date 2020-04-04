2a512edb3444433e4ca98941a85a4b1778b4eb06
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-04 18:51:41 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,7 +9,6 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create(author_params)
-    @author = Author.new(author_params)
     if @author.save
       redirect_to author_path(@author)
     else
