e0792f560485494b62b39eed26af10070ade6c52
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-04 18:52:43 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -20,6 +20,14 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+  def update
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :'/authors/edit'
+    end
+
   private
 
   def author_params
